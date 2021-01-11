ec547dee782bbf66e490574bec725f0b9a3687c3
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2021-01-11 16:47:38 +0100

    fix new column

--- docker/cron/load-data.sh
+++ docker/cron/load-data.sh
@@ -5,8 +5,17 @@ echo "Job started at $timestamp"
 #export DATE=$(date +%Y%m%d)
 #export DATE=20200309
 #curl -vvv -o /tmp/dpc-covid19-ita-regioni-${DATE}.csv https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-${DATE}.csv
-psql postgresql://postgres:postgres@postgres:5432/postgres -c "truncate \"dati-regioni\""
 #psql postgresql://postgres:postgres@postgres:5432/postgres -c "\COPY \"dati-regioni\" FROM '/tmp/dpc-covid19-ita-regioni-${DATE}.csv' WITH (FORMAT csv, DELIMITER ',', HEADER true);" 
-psql postgresql://postgres:postgres@postgres:5432/postgres -c "\COPY \"dati-regioni\" FROM PROGRAM 'curl \"https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv\"' WITH (FORMAT csv, DELIMITER ',', HEADER true);"
 #psql postgresql://postgres:postgres@postgres:5432/postgres -c "UPDATE public.\"dati-regioni\" SET data = data AT TIME ZONE 'Europe/Rome';"
 #rm -f /tmp/dpc-covid19-ita-regioni-${DATE}.csv
+
+
+#psql postgresql://postgres:postgres@postgres:5432/postgres -c "truncate \"dati-regioni\""
+#psql postgresql://postgres:postgres@postgres:5432/postgres -c "\COPY \"dati-regioni\" FROM PROGRAM 'curl \"https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv\"' WITH (FORMAT csv, DELIMITER ',', HEADER true);"
+
+
+
+psql postgresql://postgres:postgres@postgres:5432/postgres -c "truncate \"dati-regioni\""
+curl -s https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv -o /tmp/dpc-covid19-ita-regioni.csv
+psql postgresql://postgres:postgres@postgres:5432/postgres -c "\COPY \"dati-regioni\" FROM '/tmp/dpc-covid19-ita-regioni.csv' WITH (FORMAT csv, DELIMITER ',', HEADER true);" 
+rm /tmp/dpc-covid19-ita-regioni.csv
